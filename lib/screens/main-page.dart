0241fd3d0aa9133b7033f0a7c67a1fab3307f316
Thang <t7dinhn@swisslog.com>
2021-04-25 15:05:34 +0200

    Switch between progress circle icon and account delete button for slightly better UX

--- lib/screens/main-page.dart
+++ lib/screens/main-page.dart
@@ -49,58 +49,59 @@ class _MainPageState extends State<MainPage> {
                 bottom: BorderSide(width: 0.5, color: Colors.lightBlueAccent, style: BorderStyle.solid)
               ),
               columnWidths: const <int, TableColumnWidth>{
-                0: FixedColumnWidth(14),
+                0: FixedColumnWidth(20),
                 1: FixedColumnWidth(20),
-                2: FixedColumnWidth(14),
-                3: FlexColumnWidth(),
-                4: FixedColumnWidth(150),
-                5: FixedColumnWidth(50),
+                2: FlexColumnWidth(),
+                3: FixedColumnWidth(122),
+                4: FixedColumnWidth(14),
+                5: FixedColumnWidth(20),
+                6: FixedColumnWidth(14),
               },
               defaultVerticalAlignment: TableCellVerticalAlignment.middle,
               children: <TableRow>[
                 for (var otp in appModel.otpList) 
                   TableRow(children: [
-                      SizedBox(width: 10),
-                      SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(  
-                          strokeWidth: 6,  
-                          backgroundColor: Colors.grey[300],  
-                          valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),  
-                          value: otp.progress,  
+                    IconButton(icon: Icon(Icons.delete), onPressed: () => {
+                      showDialog(
+                        context: context, 
+                        builder: (_) => AlertDialog(
+                          title: Text("Remove account?", style: Theme.of(context).textTheme.headline6, textAlign: TextAlign.left,),
+                          content: Text('${otp.name}', style: Theme.of(context).textTheme.bodyText2, textAlign: TextAlign.left,),
+                          elevation: 24,
+                          actions: [
+                            TextButton(
+                              autofocus: false,
+                              child: Text("Yes", style: TextStyle(fontSize: 18)), 
+                              onPressed: () => {
+                                appModel.deleteOTP(otp.uuid),
+                                Navigator.pop(context),
+                              }
+                            ),
+                            TextButton(                      
+                              autofocus: true,
+                              child: Text("No", style: TextStyle(color: Colors.red, fontSize: 18)), 
+                              onPressed: () => Navigator.pop(context)
+                            ),
+                          ],
                         ),
+                        barrierDismissible: false,
                       ),
-                      SizedBox(width: 10),
-                      Text('${otp.name}', style: Theme.of(context).textTheme.subtitle1, textAlign: TextAlign.left,),
-                      Text('${otp.value}', style: Theme.of(context).textTheme.headline4, textAlign: TextAlign.right,),
-                      IconButton(icon: Icon(Icons.delete), onPressed: () => {
-                        showDialog(
-                          context: context, 
-                          builder: (_) => AlertDialog(
-                            title: Text("Remove account?", style: Theme.of(context).textTheme.headline6, textAlign: TextAlign.left,),
-                            content: Text('${otp.name}', style: Theme.of(context).textTheme.bodyText2, textAlign: TextAlign.left,),
-                            elevation: 24,
-                            actions: [
-                              TextButton(
-                                autofocus: false,
-                                child: Text("Yes", style: TextStyle(fontSize: 18)), 
-                                onPressed: () => {
-                                  appModel.deleteOTP(otp.uuid),
-                                  Navigator.pop(context),
-                                }
-                              ),
-                              TextButton(                      
-                                autofocus: true,
-                                child: Text("No", style: TextStyle(color: Colors.red, fontSize: 18)), 
-                                onPressed: () => Navigator.pop(context)
-                              ),
-                            ],
-                          ),
-                          barrierDismissible: false,
-                        ),
-                      }
-                    )
+                    }),
+                    SizedBox(width: 10),
+                    Text('${otp.name}', style: Theme.of(context).textTheme.subtitle1, textAlign: TextAlign.left,),
+                    Text('${otp.value}', style: Theme.of(context).textTheme.headline4, textAlign: TextAlign.right,),
+                    SizedBox(width: 10),
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: CircularProgressIndicator(  
+                        strokeWidth: 6,  
+                        backgroundColor: Colors.grey[300],  
+                        valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),  
+                        value: otp.progress,  
+                      ),
+                    ),
+                    SizedBox(width: 10),
                   ]
                 ),
               ]
